5f35c91f0b1d6804bad677fafd03993af520cfcc
sesame <1622587+sesame@users.noreply.github.com>
2025-11-13 12:55:04 +0900

    Refactor MitreAttackDataContainer to use a unified get method for data retrieval and update pyproject.toml with development dependencies

--- src/sesame_attack/mitre_attack_data_container.py
+++ src/sesame_attack/mitre_attack_data_container.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 from mitreattack.stix20.MitreAttackData import MitreAttackData
 
+
 class MitreAttackDataContainer:
     def __init(self, enterprise_attack_path, mobile_attack_path):
         self._enterprise_attack = Path(enterprise_attack_path).expanduser().resolve()
@@ -10,7 +11,26 @@ class MitreAttackDataContainer:
         self._mobile = None
 
     def enterprise(self):
-        return self._enterprise or MitreAttackData(str(self._enterprise_attack))
+        return self.get("enterprise-attack")
 
     def mobile(self):
-        return self._mobile or MitreAttackData(str(self._mobile_attack))
+        return self.get("mobile-attack")
+
+    def get(self, domain):
+        mitre_attack_data = None
+
+        match domain:
+            case "enterprise-attack":
+                mitre_attack_data = self._enterprise or MitreAttackData(
+                    str(self._enterprise_attack)
+                )
+            case "mobile-attack":
+                mitre_attack_data = self._mobile or MitreAttackData(
+                    str(self._mobile_attack)
+                )
+            case _:
+                raise ValueError(
+                    f"domain must be enterprise-attack or mobile-attack. {domain}"
+                )
+
+        return mitre_attack_data
