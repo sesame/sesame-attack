8e84ac8733eff23c9c7143b06cd7ae3aaa4bf1d1
sesame <1622587+sesame@users.noreply.github.com>
2025-11-16 06:46:43 +0900

    streamline data retrieval logic

--- src/sesame_attack/mitre_attack_data_container.py
+++ src/sesame_attack/mitre_attack_data_container.py
@@ -4,7 +4,7 @@ from mitreattack.stix20.MitreAttackData import MitreAttackData
 
 
 class MitreAttackDataContainer:
-    def __init__(self, enterprise_attack_path, mobile_attack_path):
+    def __init__(self, enterprise_attack_path, mobile_attack_path, cache_dir=None):
         self._enterprise_attack = Path(enterprise_attack_path).expanduser().resolve()
         self._mobile_attack = Path(mobile_attack_path).expanduser().resolve()
         self._enterprise = None
@@ -17,20 +17,16 @@ class MitreAttackDataContainer:
         return self.get("mobile-attack")
 
     def get(self, domain):
-        mitre_attack_data = None
-
         match domain:
             case "enterprise-attack":
-                mitre_attack_data = self._enterprise or MitreAttackData(
+                self._enterprise = self._enterprise or MitreAttackData(
                     str(self._enterprise_attack)
                 )
+                return self._enterprise
             case "mobile-attack":
-                mitre_attack_data = self._mobile or MitreAttackData(
-                    str(self._mobile_attack)
-                )
+                self._mobile = self._mobile or MitreAttackData(str(self._mobile_attack))
+                return self._mobile
             case _:
                 raise ValueError(
                     f"domain must be enterprise-attack or mobile-attack. {domain}"
                 )
-
-        return mitre_attack_data
